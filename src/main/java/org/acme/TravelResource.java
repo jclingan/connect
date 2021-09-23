55bb920cfa6162cd12e40b08c2177ee65afcc2be
jclingan <jclingan@redhat.com>
2021-09-23 12:42:21 -0700

    Initial Revision

---
+++ src/main/java/org/acme/TravelResource.java
@@ -0,0 +1,39 @@
+package org.acme;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.json.JsonObject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@RolesAllowed("user")
+@Path("/travel")
+public class TravelResource {
+  @Inject
+  Cluster cluster;
+  @ConfigProperty(name = "query")
+  String query;
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public List<JsonObject> run() {
+    // Get a reference to a particular Couchbase bucket and its default collection
+    var list = new ArrayList<JsonObject>();
+    // Perform a N1QL query
+    var queryResult = cluster.query(query);
+    queryResult.rowsAsObject().forEach(row -> {
+      list.add(row.getObject("travel-sample"));
+      System.out.println(row);
+    });
+    return list;
+  }
+}
